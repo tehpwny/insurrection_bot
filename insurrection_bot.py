de174dc693b908f15cff08d734810076e8207ce4
dʍuʎ <v.malivert@eden-3d.org>
2017-04-18 15:18:27 +0200

    removing automatic_insurrection and ruby deps

--- insurrection_bot.py
+++ insurrection_bot.py
@@ -1,72 +1,246 @@
-import sys
-
-from random import choice
-from time import sleep
-import urllib.request
-from urllib.error import URLError
-
-from bs4 import BeautifulSoup
-from subprocess import call, Popen
+"""
+# TODO
+"""
 import os
 import sys
-from bs4.element import Tag
+from time import sleep
+from subprocess import Popen
+from random import choice, randrange
 
-
-RAND = '--more-rant' in sys.argv
 PRETTY = '--pretty' in sys.argv
 
-INSURRECTIOM_SERVER = 'http://localhost:4567'
-def get_sock():
-    try:
-        sock = urllib.request.urlopen(INSURRECTIOM_SERVER).read().decode("utf-8")
-    except urllib.error.URLError:
-        raise RuntimeError("automatic_insurection server should be running on localhost:4567 (https://github.com/johm/automatic_insurrection)")
-    else:
-        return sock
+class Tiqqun:
+    words = {
+        "things_we_like": [
+            "rupture", "insurrection", "crisis", "social war",
+            "zones of indistinction which need no justification",
+            "indifference"
+        ],
+        "things_we_dont_like": [
+            "activism", "representation", "humanism",
+            "totality", "passivity", "banality",
+            "fossilization of our desires",
+            "mobilization", "impotentiality",
+            "normalization", "absence"
+        ],
+        "people_we_dont_like": [
+            "the milieu", "liberalism",
+            "the bureaucrats of revolt",
+            "anarcho-liberalism"
+        ],
+        "things_we_do": [
+            "desire", "riot", "occupy everything"
+        ],
+        "our_things": [
+            "communes", "multiplicities", "encounters",
+            "becomings", "zones of offensive opacity", "desiring-bodies"
+        ],
+        "symbolic_things": [
+            "burning dumpster", "smashed window",
+            "moment of friendship", "car set aflame",
+            "barricaded hallway"
+        ],
+        "things_we_do_to_things": [
+            "destroy", "shatter", "negate", "reject"
+        ],
+        "things_we_dont_do": [
+            "organize", "negotiate",
+            "make demands", "be productive"
+        ],
+        "how_we_do_things": [
+            "in secret", "without illusions", "for once and for all",
+            "absolutely"
+        ],
+        "describing_good_things": [
+            "singular", "immanent", "inoperative", "radical"
+        ],
+        "describing_bad_things": [
+            "homogenous", "pathetic", "compulsive"
+        ],
+        "fancy_words": [
+            "logic", "structure", "being", "temporality", "teleology"
+        ],
+        "happiness": ["joy", "ecstasy"],
+        "sadness": ["misery", "catastrophe"],
+        "really": [
+            "by any means necessary", "with every weapon at our disposal",
+            "without looking back", "at all costs"
+        ],
+        "making_things": [
+            "articulation", "construction", "elaboration", "setting forth",
+            "realization"
+        ],
+        "plans": ["plan", "project", "concept"],
+        "antiplans": [
+            "a *state of exception*", "a *line of flight*",
+            "an *event*"
+        ],
+        "events": ["orgies", "festivals", "conspiracies"],
+        "fun_stuff": ["destruction", "negation"],
+        "get_along": ["dialogue", "criticism", "sympathy"],
+        "go_away": ["scorn", "contempt", "derision"],
+        "dont_do": ["refuse", "neglect", "fail"],
+        "preposition": ["on", "towards"]
+    }
 
-def get_rants():
-    url = 'http://localhost:4567'
-    sock = get_sock()
+    def get_word(self, key):
+        return self.words.get(key)
 
-    soup = BeautifulSoup(sock, "html.parser")
+    def get_words(self, keys):
+        for key in keys:
+            yield self.words.get(key)
 
-    rant_text = soup.find('div', id='rant').text
-    return rant_text.split('. ')
+    def get_rand_word(self, key):
+        return choice(self.words.get(key))
 
-def get_rant():
+    def get_rand_words(self, *keys):
+        already_said = []
+        chosen = None
+        for key in keys:
+            words = self.words.get(key)
+            if words is not None:
+                while chosen in already_said or chosen is None:
+                    chosen = choice(words)
+                already_said.append(chosen)
+                yield chosen
 
-    sock = get_sock()
+    def rant(self):
+        rants = [
+            self.recognize,
+            self.do_something,
+            self.in_the,
+            self.break_things,
+            self.this_call,
+            self.whats_needed,
+            self.every_what,
+            self.joke,
+            self.necessary,
+            self.symbols
+        ]
+        rant = choice(rants)
+        return str(rant()).replace("\n", " ")
 
-    soup = BeautifulSoup(sock, "html.parser")
-    div_rant = soup.find('div', id='rant').find('div', id='pull_quote')
-    rant = div_rant.contents
 
-    rant_msg = ['*{}*'.format(element.contents[0]) if isinstance(element, Tag) else element for element in rant]
+    def recognize(self):
+        return """Confronted with those who {} to recognize themselves in our {} of {}, we offer neither {} nor {} but only our {}.""".format(
+            *self.get_rand_words("dont_do", "events", "fun_stuff", "get_along",
+                                "get_along", "go_away")
+        )
 
-    div_morerant = soup.find('div', id='morerant')
-    morerant = div_morerant.contents
-    morerant_msg = ['*{}*'.format(element.contents[0]) if isinstance(element, Tag) else element for element in morerant]
-    msg = choice((' '.join(rant_msg), ' '.join(morerant_msg))) if RAND else ' '.join(rant_msg)
-    return msg.replace('*', '')
 
+    def do_something(self):
+        return """Our need to {} is less the {} of a {} than the {} of {}.""".format(*self.get_rand_words(
+            "things_we_do",
+            "making_things",
+            "plans",
+            "making_things",
+            "antiplans"
+        ))
+
+
+    def in_the(self):
+        return """In the {} of {}, we {} those who would have us give up the {} {} of {} for the {} of {}.""".format(*self.get_rand_words(
+            "making_things", "our_things", "things_we_do_to_things",
+            "describing_good_things", "happiness", "things_we_like",
+            "sadness", "things_we_dont_like"
+        ))
+
+
+    def title(self):
+        return """Leaving {} behind: Notes {} {}
+        """.format(*self.get_rand_words(
+            "things_we_dont_like", "preposition", "things_we_like"
+        ))
+
+
+    def break_things(self):
+      return """We must {} all {} *{}*.
+      """.format(*self.get_rand_words("things_we_do_to_things", "things_we_dont_like", "how_we_do_things"))
+
+
+    def this_call(self):
+       return """This is a call to {}, not an insistence on {}.
+       """.format(*self.get_rand_words("things_we_like", "things_we_dont_like"))
+
+
+    def whats_needed(self):
+      return """What's needed is not {}, and even far less {}, but a putting-into-practice of {} {}, a rejection in all forms of the {} of {}.
+      """.format(*self.get_rand_words("things_we_dont_like",
+                                     "things_we_dont_like",
+                                     "describing_good_things",
+                                     "things_we_like",
+                                     "fancy_words",
+                                     "things_we_dont_like"
+      ))
+
+
+    def every_what(self):
+      return """Every {} is a refusal to {}, a blow against the {} of {}, a recognition of the {} {} inherent in the articulation of {}.
+      """.format(*self.get_rand_words(
+          "symbolic_things",
+          "things_we_dont_do",
+          "fancy_words",
+          "people_we_dont_like",
+          "describing_good_things",
+          "fancy_words",
+          "our_things"
+      ))
+
+
+    def joke(self):
+      return """The {} {} proposed to us is like a bad joke, and instead of laughter we respond with {}.
+      """.format(*self.get_rand_words(
+          "describing_bad_things",
+          "things_we_dont_like",
+          "things_we_like"
+      ))
+
+
+    def necessary(self):
+      return """It is necessary to commence {} to dream of new ways to {}, but to make manifest the subterranean {} in the heart of each {}.
+      """.format(*self.get_rand_words(
+          "how_we_do_things",
+          "things_we_dont_do",
+          "our_things",
+          "symbolic_things"
+      ))
+
+
+    def symbols(self):
+      return """To those who deride the {} {} in a {} or a {}, we propose nothing less than to {} their {} {}, {}.
+      """.format(*self.get_rand_words(
+          "describing_good_things",
+          "happiness",
+          "symbolic_things",
+          "symbolic_things",
+          "things_we_do_to_things",
+          "describing_bad_things",
+          "things_we_dont_like",
+          "really"
+      ))
+
+
+appelist = Tiqqun()
 
 if '--fortune' in sys.argv:
     if PRETTY:
-        print('\n*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n\n{}\n'.format(get_rant()))
+        print('\n*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n\n{}\n'.format(appelist.rant()))
         print('\n*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n')
     else:
-        print(get_rant())
+        print(appelist.rant())
     sys.exit()
 
 if '--trap' in sys.argv:
     beat = Popen(["mplayer", "-loop", "0", "-really-quiet", "./trap.m4a"])
 
 
+print('\n\nScolie {} \n\t - {} \n'.format(randrange(1, 1312), appelist.title()))
+
 while True:
     sleep(4)  # wait for it.
     print('\n*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\n\n\n')
-    for message in get_rants():
-        print(message + '\n')
-        cmd = 'espeak -p 45 -s 172 "{}"'.format(message)
-        sleep(choice((0.75, 1)))
-        os.system(cmd)
+    message = appelist.rant()
+    print(message + '\n')
+    cmd = 'espeak -p 45 -s 172 "{}"'.format(message)
+    sleep(choice((0.75, 1)))
+    # os.system(cmd)
